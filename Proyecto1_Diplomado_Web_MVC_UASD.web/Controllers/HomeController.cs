d38047fdde57ef40b2a2d4a44ed12e7c1dadbd13
Jonathan Lorenzo Contrera <naruto_rtx@hotmail.com>
2019-11-27 22:40:13 -0400

    Add project files.

---
+++ Proyecto1_Diplomado_Web_MVC_UASD.web/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto1_Diplomado_Web_MVC_UASD.web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "<html> <body>" +
+                "<h1>Universidad Autonoma de Santo Domingo (UASD)</h1>" +
+                "<p>Diplomado Desarrollo Web C#, MVC </p>" +
+                "</body></html>";
+        }
+
+        public string DiplomadoWeb()
+        {
+            return "<html> <body>" +
+                "<h1>Estudiantes:</h1>" +
+                "<p>Yorqui Montero Sanchez<br>" +
+                "Junior Maria Araujo<br>" +
+                "Marileidy Manzueta de la Rosa<p>" +
+                "</body></html>";
+        }
+    }
+}
---
+++ Proyecto1_Diplomado_Web_MVC_UASD.web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Proyecto1_Diplomado_Web_MVC_UASD.web.MvcApplication" Language="C#" %>
