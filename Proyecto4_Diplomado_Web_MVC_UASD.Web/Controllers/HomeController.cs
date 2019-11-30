bdc179aacf5f27eddd7434003207c8dbf028a065
Jonathan Lorenzo Contrera <naruto_rtx@hotmail.com>
2019-11-29 21:47:44 -0400

    Actualizacion de Proyecto 4

---
+++ Proyecto4_Diplomado_Web_MVC_UASD.Web/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using Proyecto4_Diplomado_Web_MVC_UASD.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto4_Diplomado_Web_MVC_UASD.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FormularioVisita()
+        {
+            return View();
+        }
+
+        public ActionResult CargaDatos()
+        {
+            string nombre = "";
+            string comentarios = "";
+
+            nombre = Request.Form["nombre"].ToString();
+            comentarios = Request.Form["comentarios"].ToString();
+
+            LibroVisitas libro = new LibroVisitas();
+            libro.Grabar(nombre, comentarios);
+            return View();
+        }
+
+        public ActionResult ListadoVisitas()
+        {
+            LibroVisitas libro = new LibroVisitas();
+            string todo = libro.Leer();
+            ViewData["libro"] = todo;
+
+            return View();
+        }
+
+        public ActionResult Persona(Persona model)
+        {
+            return View();   
+        }
+
+        public ActionResult RegistraPersona()
+        {
+            Persona persona = new Persona();
+            persona.Nombre = Request.Form["Nombre"].ToString();
+            persona.Cedula = Request.Form["Cedula"].ToString();
+            persona.Telefono = Request.Form["Telefono"].ToString();
+            persona.Correo = Request.Form["Correo"].ToString();
+
+            persona.Regristrar(persona);
+            return View();
+        }
+
+        public ActionResult ListarPersonas()
+        {
+            Persona persona = new Persona();
+            string todo = persona.LeerPersonas();
+            ViewData["lista"] = todo;
+
+            return View();
+        }
+    }
+}
---
+++ Proyecto4_Diplomado_Web_MVC_UASD.Web/Models/Persona.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+
+namespace Proyecto4_Diplomado_Web_MVC_UASD.Web.Models
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public string Cedula { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+
+        
+        public void Regristrar(Persona persona)
+        {
+            StreamWriter archivo = new StreamWriter(HostingEnvironment.MapPath("~") + "/App_Data/persona.txt", true);
+            archivo.WriteLine("<tr> <td>" + persona.Nombre + "</td>" + 
+                "<td>" + persona.Cedula + "</td>" +
+                "<td>"+ persona.Telefono + "</td>" +
+                "<td>" + persona.Correo + "</td></tr>");
+            archivo.Close();
+        }
+
+        public string LeerPersonas()
+        {
+            StreamReader archivo = new StreamReader(HostingEnvironment.MapPath("~") + "/App_Data/persona.txt");
+            string todo = archivo.ReadToEnd();
+            archivo.Close();
+
+            return todo;
+           
+        }
+    }
+}
